a4235fc5b9ff04781de8101416fbe49ea659da07
alxsrobert <alexis3.robert3@gmail.com>
2021-04-13 11:38:03 +0100

    Functions to generate the simulated data

---
+++ R/function_generate_all_outbreaks.R
@@ -0,0 +1,71 @@
+generate_all_sim <- function(n_sim, w_dens, cov, area, pop, list_params,
+                             distance_matrix, start = as.Date("2009-01-29")){
+  # Create a list where each element will be a matrix containing the number
+  # of cases per region per day in a given simulation.
+  list_sim <- vector(mode = "list", length = n_sim)
+  # Compute the number of parameters
+  n_params <- length(list_params$mean)
+  # Update the names of the parameters
+  names_params <- c("intercept_ar", "vacc_ar", "cat1_imm_ar", "cat2_imm_ar",
+                    "pop_ar", "area_ar", "sin_ar", "cos_ar", 
+                    "intercept_ne", "vacc_ne", "cat1_imm_ne", "cat2_imm_ne",
+                    "pop_ne", "sin_ne", "cos_ne", 
+                    "intercept_en", "vacc_en", "cat1_imm_en", "cat2_imm_en",
+                    "pop_en", "sin_en", "cos_en", 
+                    "dist", "pop", "overdisp")
+  # Data frame of the parameters for each simulation
+  params_sim <- as.data.frame(matrix(nrow = n_sim, ncol = n_params))
+  # Initialise vector of parameters
+  params_current <- numeric(n_params)
+  colnames(params_sim) <- names(params_current) <- names_params
+  # Sort the different data sources so that they have the same column order
+  area <- area[, colnames(cov)]
+  pop <- pop[, colnames(cov)]
+  distance_matrix <- distance_matrix[colnames(cov), colnames(cov)]
+  # 3 Data frames of the final value of the predictor for each region 
+  r_fin_ar <- r_fin_ne <- r_fin_end <- 
+    as.data.frame(matrix(nrow = n_sim, ncol = nrow(distance_matrix)))
+  colnames(r_fin_ar) <- colnames(r_fin_ne) <- colnames(r_fin_end) <- 
+    colnames(distance_matrix)
+  
+  for(i in 1:n_sim){
+    # Draw the parameter sets using the covariance matrix
+    dt_params <- list_params[[1]] + (list_params[[2]] %>% chol %>% t)%*%
+      matrix(rnorm(n = ncol(list_params[[2]])),
+             nrow = ncol(list_params[[2]]), ncol = 1)
+    # Update params_current
+    params_current[] <- dt_params[1:n_params,1]
+    params_current["overdisp"] <- exp(params_current["overdisp"])
+    # Run the simulation
+    outbreak <- generate_sim(params = params_current, pop = pop, cov = cov, 
+                             area = area, w_dens = w_dens, 
+                             start = as.Date("2009-01-01"),  
+                             distance_matrix = distance_matrix)
+    # If more than 100,000 cases in ten years, re-simulate
+    while(sum(outbreak$n_cases > 100000)){
+      dt_params <- list_params[[1]] + (list_params[[2]] %>% chol %>% t)%*%
+        matrix(rnorm(n = ncol(list_params[[2]])),
+               nrow = ncol(list_params[[2]]), ncol = 1)
+      params_current[] <- dt_params[1:n_params,1]
+      
+      params_current["overdisp"] <- exp(params_current["overdisp"])
+      outbreak <- generate_sim(params = params_current, pop = pop, cov = cov, 
+                               area = area, w_dens = w_dens, 
+                               start = start,  
+                               distance_matrix = distance_matrix)
+    }
+    # Extract the number of cases per day per region
+    list_sim[[i]] <- outbreak$n_cases
+    # Extract the final value of the autoregressive predictor per region
+    r_fin_ar[i,] <- outbreak$r0_ar[nrow(outbreak$r0_ar),]
+    # Extract the final value of the neighbourhood predictor per region
+    r_fin_ne[i,] <- outbreak$r0_ne[nrow(outbreak$r0_ne),]
+    # Extract the final value of the endemic predictor per region
+    r_fin_end[i,] <- outbreak$r0_en[nrow(outbreak$r0_en),]
+    # Extract the parameter set for this simulation
+    params_sim[i,] <- params_current
+  }
+  
+  return(list(sim = list_sim, r_ar = r_fin_ar, r_ne = r_fin_ne,
+              r_end = r_fin_end, params_sim = params_sim))
+}
---
+++ R/function_generate_outbreak.R
@@ -0,0 +1,181 @@
+generate_sim <- function(w_dens, cov, area, pop, distance_matrix, 
+                         params, start, thresh_immun = 45){
+  # Set up intercept of each component
+  R0_ar <- rep(params["intercept_ar"], ncol(cov))
+  R0_en <- rep(params["intercept_en"], ncol(cov))
+  R0_ne <- rep(params["intercept_ne"], ncol(cov))
+  names(R0_ar) <- names(R0_en) <- names(R0_ne) <- colnames(cov)
+  
+  ## Compute distance matrix
+  if(!is.matrix(pop)){
+    # If the number of inhabitants is constant throughout the study period
+    # Exponential gravity model
+    wji <- exp(-distance_matrix*(params["dist"])) * (pop^params["pop"]) / pop
+    # Distance between a region and itself is 0
+    wji[distance_matrix == 0] <- 0
+  } else{
+    # If the number of inhabitants varies throughout the study period
+    # Exponential gravity model applied for each value of pop
+    wji <- apply(pop, 1, function(X){
+      if(any(names(params) == "dist")){
+        wji_X <- exp(-distance_matrix*(params["dist"])) * (X^params["pop"]) / X 
+      } else {
+        wji_X <- (X^params["pop"]) / X
+      }
+      # Distance between a region and itself is 0
+      wji_X[distance_matrix == 0] <- 0
+      return(list(wji_X))})
+    # REturn the first value to avoid having a list within a list
+    wji <- lapply(wji, function(X) return(X[[1]]))
+  }
+  # Extract the first value for each covariate
+  cov_0 <- cov[as.character(start), ]
+  pop_0 <- pop[as.character(start),]
+  area_0 <- area[as.character(start),]
+  
+  # At the starting point, the level of recent incidence is 0 in each region 
+  immun_0 <- rep(1, length(cov_0))
+  immun_1 <- rep(0, length(cov_0))
+  immun_2 <- rep(0, length(cov_0))
+  names(immun_0) <- names(immun_1) <- names(immun_2) <- names(cov_0)
+
+  # Compute the number of iterations
+  weeks_sim <- rownames(cov_ts)[rownames(cov_ts) >= start]
+  # Convert to numeric
+  num_dates <- as.numeric(as.Date(weeks_sim))
+  
+  # Initialise the matrix containing the number of cases per day per region
+  n_cases <- matrix(nrow= length(weeks_sim), ncol = ncol(cov))
+  rownames(n_cases) <- weeks_sim
+  colnames(n_cases) <- colnames(cov)
+  
+  # Initialise the matrices with the values of the predictor per day per region
+  all_r0_ar <- matrix(nrow = length(weeks_sim), ncol = ncol(cov))
+  all_r0_ne <- matrix(nrow = length(weeks_sim), ncol = ncol(cov))
+  all_r0_en <- matrix(nrow = length(weeks_sim), ncol = ncol(cov))
+  
+  # Compute the first value of each predictor
+  t <- 0
+  R0_ar_reg <- exp(R0_ar + params["vacc_ar"] * log(1 - cov_0/100) + 
+                     params["cat1_imm_ar"] * immun_1 + 
+                     params["cat2_imm_ar"] * immun_2 + 
+                     params["pop_ar"] * log(pop_0 / 1000000) + 
+                     params["area_ar"] * log(area_0) + 
+                     params["sin_ar"] * sin(t * 2*pi/365)+
+                     params["cos_ar"] * cos(t * 2*pi/365))
+  R0_ne_reg <- exp(R0_ne + params["vacc_ne"] * log(1 - cov_0/100) + 
+                     params["cat1_imm_ne"] * immun_1 + 
+                     params["cat2_imm_ne"] * immun_2 + 
+                     params["pop_ne"] * log(pop_0 / 1000000) + 
+                     params["sin_ne"] * sin(t * 2*pi/365)+
+                     params["cos_ne"] * cos(t * 2*pi/365))
+  R0_en_reg <- exp(R0_en + params["vacc_en"] * log(1 - cov_0/100) + 
+                     params["cat1_imm_en"] * immun_1 + 
+                     params["cat2_imm_en"] * immun_2 + 
+                     params["pop_en"] * log(pop_0 / 1000000) + 
+                     params["sin_en"] * sin(t * 2*pi/365)+
+                     params["cos_en"] * cos(t * 2*pi/365))
+  # Add the initial values of the predictors to the predictor matrices
+  all_r0_ar[1,] <- R0_ar_reg
+  all_r0_ne[1,] <- R0_ne_reg
+  all_r0_en[1,] <- R0_en_reg
+  
+  # Number of cases at t=0 is 0
+  n_cases_ar <- n_cases_en <- n_cases_ne <- immun <- n_cases
+  # Draw the number of cases at t = 1
+  n_cases[t + 1,] <- sapply(R0_en_reg, function(X)
+    rnbinom(1, mu = X, size = params["overdisp"]))
+  # All cases at t=1 come from the endemic component
+  n_cases_en[t + 1,] <- R0_en_reg
+  
+  for(i in seq_along(weeks_sim)[-1]){
+    # Increase t by 1
+    t <- t + 1
+    time_i <- rownames(n_cases)[i]
+    # Update the value of the covariates
+    cov_i <- cov[t + 1, ]
+    area_i <- area[t + 1,]
+    pop_i <- pop[t + 1,]
+
+    ## Compute the level of recent transmission:
+    if(as.Date(time_i) - as.Date(weeks_sim[1]) > 30){
+      # Compute the numeric value of the current date since numeric comparison 
+      # is faster
+      num_date <- as.numeric(as.Date(time_i))
+      ## For each region, sum cases infected 1 month to 1 year before time_i
+      # generate dates
+      weeks_immun <- 
+        rownames(n_cases)[num_dates < num_date - 30 &
+                            num_dates > num_date - 365 * 3]
+      # Select rows
+      immunity <- n_cases[weeks_immun,]
+      # Sum rows
+      if(length(weeks_immun) > 1) immunity <- colSums(n_cases[weeks_immun,]) 
+      
+      # Compute the number of cases per million
+      immunity <- immunity / pop_i * 1000000
+      immun_0 <- immun_1 <- immun_2 <- immunity * 0
+      # Region with a number of cases per million below 10 belong to cat0
+      immun_0[immunity < 10] <- 1
+      # Region with a number of cases per million 10-thresh belong to cat1
+      immun_1[immunity > 10 & immunity <= thresh_immun] <- 1
+      # Region with a number of cases per million above thresh belong to cat2
+      immun_2[immunity > thresh_immun] <- 1
+      if(any(immun_0 + immun_1 + immun_2 !=1)) stop("Problem immunity")
+    }
+    # Compute the value of the predictors at time_i
+    R0_ar_reg <- exp(R0_ar +
+                       params["vacc_ar"] * log(1 - cov_i/100) + 
+                       params["cat1_imm_ar"] * immun_1 + 
+                       params["cat2_imm_ar"] * immun_2 + 
+                       params["pop_ar"] * log(pop_i / 1000000) + 
+                       params["area_ar"] * log(area_i) + 
+                       params["sin_ar"] * sin(t * 2*pi/365)+
+                       params["cos_ar"] * cos(t * 2*pi/365))
+    R0_ne_reg <- exp(R0_ne +
+                       params["vacc_ne"] * log(1 - cov_i/100) + 
+                       params["cat1_imm_ne"] * immun_1 + 
+                       params["cat2_imm_ne"] * immun_2 + 
+                       params["pop_ne"] * log(pop_i / 1000000) + 
+                       params["sin_ne"] * sin(t * 2*pi/365)+
+                       params["cos_ne"] * cos(t * 2*pi/365))
+    R0_en_reg <- exp(R0_en +
+                       params["vacc_en"] * log(1 - cov_i/100) + 
+                       params["cat1_imm_en"] * immun_1 + 
+                       params["cat2_imm_en"] * immun_2 + 
+                       params["pop_en"] * log(pop_i/ 1000000) + 
+                       params["sin_en"] * sin(t * 2*pi/365)+
+                       params["cos_en"] * cos(t * 2*pi/365))
+    # Update the matrices
+    all_r0_ar[i,] <- R0_ar_reg
+    all_r0_ne[i,] <- R0_ne_reg
+    all_r0_en[i,] <- R0_en_reg
+    ## Compute the infection potential using the serial interval and the number
+    ## of cases in the last 50 days
+    diff_time <- (seq_len(min(50, i - 1))) + max(0, i - 1 -50)
+    cases_tot <- (n_cases[diff_time,] * 
+                    matrix(w_dens[length(diff_time):1], 
+                           nrow = length(diff_time), ncol = ncol(n_cases), 
+                           byrow = F)) %>% colSums()
+    names(cases_tot) <- colnames(n_cases)
+    # Compute the average number of new cases per component
+    new_ar_av <- R0_ar_reg * cases_tot
+    new_en_av <- R0_en_reg
+    if(!is.list(wji)) new_ne_av <- R0_ne_reg * ((cases_tot * wji) %>% colSums()) else
+      new_ne_av <- R0_ne_reg * ((cases_tot * wji[[i-1]]) %>% colSums())
+    # Draw the number of cases at time_i per region
+    n_cases[i,] <- sapply(new_en_av + new_ar_av + new_ne_av, function(X)
+      rnbinom(1, mu = X, size = params["overdisp"]))
+    # Update the matrices of the average number of cases per component
+    n_cases_ar[i,] <- new_ar_av
+    n_cases_en[i,] <- new_en_av
+    n_cases_ne[i,] <- new_ne_av
+    # Update the matrix of the level of coverage per day per region
+    immun[i,] <- immun_1 + immun_2 * 2
+  }
+  
+  return(list(n_cases = n_cases, immun = immun,
+              r0_ar = all_r0_ar, r0_ne = all_r0_ne, r0_en = all_r0_en, 
+              n_cases_ar = n_cases_ar, n_cases_en = n_cases_en, 
+              n_cases_ne = n_cases_ne))
+}
