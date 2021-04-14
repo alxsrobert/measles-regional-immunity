324197f62af972169efc508bacbf23bbabd65f3d
alxsrobert <alexis3.robert3@gmail.com>
2021-04-14 18:13:41 +0100

    Add comment

--- R/generate_simulations.R
+++ R/generate_simulations.R
@@ -4,13 +4,13 @@ source("R/import_library.R")
 source("R/import_covariates.R")
 
 ##### Generate simulations
-
+# Import set of parameters and covariance matrix
 list_params <- readRDS("Data/params_model_day_tot.RDS")
 
-w_dens <- serial_interval(prop_gen1 = 1, mean_w1 = 11.7, sd_w1 = 2.0)
+# Simulate 100 outbreaks
 set.seed(1)
 list_all_sim <- generate_all_sim(n_sim = 100, w_dens = w_dens, cov = cov_ts, 
                                  pop = pop_ts, distance_matrix = dist_mat_cent, 
                                  list_params = list_params, area = area_ts)
-
+# Save the outbreak list
 saveRDS(list_all_sim, "Output/simulation_set.RDS")
--- R/import_covariates.R
+++ R/import_covariates.R
@@ -1,25 +1,36 @@
+# Function to generate population centroids 
 source("R/function_location_centroids.R")
-
+# Functions to generate the covariate time series and the serial interval
 source("R/function_coverage.R")
 source("R/function_distance_population.R")
 source("R/function_serial_interval.R")
-
+# Functions to simulate the outbreaks
 source("R/function_generate_outbreak.R")
 source("R/function_generate_all_outbreaks.R")
+# Functions to analyse the outbreaks 
+source("R/function_exp_gravity.R")
+source("R/function_report_rate.R")
+source("R/function_prepare_data_hhh4.R")
+source("R/function_analysis_hhh4.R")
 
-
+# Import data table linking region nb and region id 
 corres <- data.table(read.csv2(file = "Data/nuts_to_dep.csv", sep = ",", 
                                dec = ".", stringsAsFactors = F)[,c(2,1)])
-
 colnames(corres) <- c("GeoCode", "regions")
 setkey(corres, regions)
-
+# Create covariate time series
 cov_ts <- importation_coverage(corres = corres)
 regs <- colnames(cov_ts)
 all_dates <- as.Date(rownames(cov_ts))
-
+# Create distance matrix using population centroids
 dist_mat_cent <- importation_distance(regs = regs, neighbours = F)
+# Create distance matrix using neighbours
+dist_mat_nei <- importation_distance(regs = regs, neighbours = T)
+# Create population and area time series
 list_pop_area <- importation_pop_area(corres = corres, regs = regs, 
                                       all_dates = all_dates)
 pop_ts <- list_pop_area$pop
 area_ts <- list_pop_area$area
+
+# Generate serial interval
+w_dens <- serial_interval(prop_gen1 = 1, mean_w1 = 11.7, sd_w1 = 2.0)
