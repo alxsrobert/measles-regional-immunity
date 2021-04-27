7cc0f994e6b5b42c51e98098a1d6d0ae03d86b41
alxsrobert <alexis3.robert3@gmail.com>
2021-04-27 16:56:20 +0100

    Add figures sensitivity analysis

--- R/function_Supplement_figures.R
+++ R/function_Supplement_figures.R
@@ -215,6 +215,135 @@ figures_coverage <- function(corres, len_break = 1,
   plot(gg3)
 }  
 
+figures_sensitivity <- function(model, mat_coef, min_coef = NULL, 
+                                max_coef = NULL,ar_labs = NULL, ne_labs = NULL,
+                                end_labs = NULL, other_labs = NULL){
+  # Extract minimum and maximum values of the 95% CI in the sensitivity analyses
+  if(!is.null(min_coef) & !is.null(max_coef)){
+    min_point <- apply(min_coef, 2, min)
+    max_point <- apply(max_coef, 2, max)
+  } 
+  
+  # Extract and sort the coefficients by components
+  ar_coef1 <- grepl("ar\\.", colnames(mat_coef)) %>% which
+  ne_coef1 <- grepl("ne\\.", colnames(mat_coef)) %>% which
+  end_coef1 <- grepl("end\\.", colnames(mat_coef)) %>% which
+  other1 <- which(!is.element(seq_len(ncol(mat_coef)), 
+                              c(ar_coef1, ne_coef1, end_coef1)))
+  # Exclude the intercepts
+  ar_coef1 <- ar_coef1[-1]
+  ne_coef1 <- ne_coef1[-1]
+  end_coef1 <- end_coef1[-1]
+  
+  ## Plot the coefficients from the autoregressive component
+  figs <- cbind(c(0, 0.65, 0, 0.5), c(0.5, 1, 0.5, 1),
+                c(0.65, 1, 0, 0.5), c(0, 0.5, 0.5, 1))
+  par(fig = figs[,4], mar = c(3, 5, 2, 1), las = 1, bty ="l", cex.axis = 1.1,
+      cex.main = 1.5, cex.lab = 1.5)
+  y1 <- seq_along(ar_coef1) - .1
+  plot(coef(model)[ar_coef1] ~ y1, pch = 16, cex = 1.5, main = "Autoregressive", 
+       ylab = "Effect", xaxt = "n", col = "grey", ylim = c(-.5, .5),
+       xlim = c(y1[1] - .5, rev(y1)[1] + .5))
+  arrows(x0 = y1, y0 = confint(model)[ar_coef1, 1],
+         x1 = y1, y1 = confint(model)[ar_coef1, 2], 
+         angle = 90, code = 3, length = 0.05, col = "grey", lwd = 2)
+  ## Add the mean coefficients for each entry of the sensitivity analyses
+  points(c(mat_coef[,ar_coef1]) ~ rep(y1 + .1, each = nrow(mat_coef)), 
+         pch = 16, cex = 1.5, col = "#fc8d59")
+  ## Add the extreme values of the 95%CIs of the sensitivity analyses
+  if(!is.null(min_coef) & !is.null(max_coef))
+    arrows(x0 = y1 +.1, y0 = min_point[ar_coef1],
+           x1 = y1 +.1, y1 = max_point[ar_coef1], 
+           angle = 90, code = 3, length = 0.05, col = "orange", lwd = 1)
+  # Add the axis labels
+  if(!is.null(ar_labs)){
+    axis(side = 1, at = seq_along(ar_coef1), labels = ar_labs)    
+  } else
+    axis(side = 1, at = seq_along(ar_coef1), 
+         labels = gsub("ar.", "", names(coef(model))[ar_coef1]))
+  abline(h = 0, lty = 2)
+  
+  ## Plot the coefficients from the neighbourhood component
+  y1 <- seq_along(ne_coef1) - .1
+  par(fig = figs[,2], mar = c(3, 5, 2, 1), las = 1, bty ="l", new = T, cex.axis = 1.2,
+      cex.main = 1.5, cex.lab = 1.5)
+  plot(coef(model)[ne_coef1] ~ y1, pch = 16, cex = 1.5, main = "Neighbourhood", 
+       ylab = "Effect", xaxt = "n", ylim = c(-.5, 1.2), col = "grey", 
+       xlim = c(y1[1] - .5, rev(y1)[1] + .5))
+  arrows(x0 = y1, y0 = confint(model)[ne_coef1, 1],
+         x1 = y1, y1 = confint(model)[ne_coef1, 2], 
+         angle = 90, code = 3, length = 0.05, col = "grey", lwd = 2)
+  ## Add the mean coefficients for each entry of the sensitivity analyses
+  points(c(mat_coef[,ne_coef1]) ~ rep(y1 + .1, each = nrow(mat_coef)), 
+         pch = 16, cex = 1.5, col = "#fc8d59")
+  ## Add the extreme values of the 95%CIs of the sensitivity analyses
+  if(!is.null(min_coef) & !is.null(max_coef))
+    arrows(x0 = y1 +.1, y0 = min_point[ne_coef1],
+           x1 = y1 +.1, y1 = max_point[ne_coef1], 
+           angle = 90, code = 3, length = 0.05, col = "orange", lwd = 1)
+  # Add the axis labels
+  if(!is.null(ne_labs)){
+    axis(side = 1, at = seq_along(ne_coef1), labels = ne_labs)
+  } else
+    axis(side = 1, at = seq_along(ne_coef1), 
+         labels = gsub("ne.", "", names(coef(model))[ne_coef1]))
+  abline(h = 0, lty = 2)
+  
+  ## Plot the coefficients from the endemic component
+  y1 <- seq_along(end_coef1) - .1
+  par(fig = figs[,1], mar = c(3, 5, 2, 1), las = 1, bty ="l", new = T, cex.axis = 1.2,
+      cex.main = 1.5, cex.lab = 1.5)
+  plot(coef(model)[end_coef1] ~ y1, pch = 16, cex = 1.5, main = "Endemic", ylab = "Effect", 
+       xaxt = "n", xlim = c(y1[1] - .5, rev(y1)[1] + .5), ylim = c(-1, 2), 
+       col = "grey")
+  arrows(x0 = y1, y0 = confint(model)[end_coef1, 1],
+         x1 = y1, y1 = confint(model)[end_coef1, 2], 
+         angle = 90, code = 3, length = 0.05, col = "grey", lwd = 2)
+  ## Add the mean coefficients for each entry of the sensitivity analyses
+  points(c(mat_coef[,end_coef1]) ~ rep(y1 + .1, each = nrow(mat_coef)),
+         pch = 16, cex = 1.5, col = "#fc8d59")
+  ## Add the extreme values of the 95%CIs of the sensitivity analyses
+  if(!is.null(min_coef) & !is.null(max_coef))
+    arrows(x0 = y1 +.1, y0 = min_point[end_coef1],
+           x1 = y1 +.1, y1 = max_point[end_coef1], 
+           angle = 90, code = 3, length = 0.05, col = "orange", lwd = 1)
+  # Add the axis labels
+  if(!is.null(end_labs)){
+    axis(side = 1, at = seq_along(end_coef1), labels = end_labs)    
+  } else
+    axis(side = 1, at = seq_along(end_coef1), 
+         labels = gsub("end.", "", names(coef(model))[end_coef1]))
+  abline(h = 0, lty = 2)
+  
+  ## Plot the other coefficients (gravity model and overdispersion)
+  y1 <- seq_along(other1) - .1
+  par(fig = figs[,3], mar = c(3, 5, 2, 1), las = 1, bty ="l", new = T, cex.axis = 1.2,
+      cex.main = 1.5, cex.lab = 1.5)
+  plot(coef(model)[other1] ~ y1, pch = 16, cex = 1.5, main = "Others", ylab = "Effect", 
+       xaxt = "n", ylim = c(0, .8), col = "grey", 
+       xlim = c(y1[1] - .5, rev(y1)[1] + .5))
+  # Add the 95% Confidence Intervals
+  arrows(x0 = y1, y0 = confint(model)[other1, 1],
+         x1 = y1, y1 = confint(model)[other1, 2], 
+         angle = 90, code = 3, length = 0.05, col = "grey", lwd = 2)
+  ## Add the mean coefficients for each entry of the sensitivity analyses
+  points(c(mat_coef[,other1]) ~ rep(y1 + .1, each = nrow(mat_coef)),
+         pch = 16, cex = 1.5, col = "#fc8d59")
+  ## Add the extreme values of the 95%CIs of the sensitivity analyses
+  if(!is.null(min_coef) & !is.null(max_coef))
+    arrows(x0 = y1 +.1, y0 = min_point[other1],
+           x1 = y1 +.1, y1 = max_point[other1], 
+           angle = 90, code = 3, length = 0.05, col = "orange", lwd = 1)
+  # Add the axis labels
+  if(!is.null(other_labs)){
+    axis(side = 1, at = seq_along(other1), labels = other_labs)    
+  } else
+    axis(side = 1, at = seq_along(other1), 
+         labels = gsub("neweights.", "", names(coef(model))[other1]))
+  
+}
+
+## Plot the seasonality in the compartments of both models
 figure_s9 <- function(tot, nei){
   ## Extract the two seasonality coefficients for each component:
   #-in tot
@@ -266,19 +395,25 @@ figure_s9 <- function(tot, nei){
   
 }
 
+## Generate the proportion of cases coming from each component in the models
 figure_s11 <- function(tot, nei){
+  ### Generate the average number of cases per component at each date
+  ## In model "tot"
   tot_hhh <- meanHHH(coef(tot), terms(tot))
   tot_nb <- c(Endemic = sum(tot_hhh$endemic), 
               Autoregressive = sum(tot_hhh$epi.own),
               Neighbour = sum(tot_hhh$epi.neighbours))
+  # Generate the proportion
   tot_prop <- tot_nb/sum(tot_nb) * 100
+  ## In model "nei"
   nei_hhh <- meanHHH(coef(nei), terms(nei))
   nei_nb <- c(Endemic = sum(nei_hhh$endemic), 
               Autoregressive = sum(nei_hhh$epi.own),
               Neighbour = sum(nei_hhh$epi.neighbours))
+  # Generate the proportion
   nei_prop <- nei_nb/sum(nei_nb) * 100
   
-  
+  # Plot both proportions
   par(mfrow = c(1,1), mar = c(5, 5, 1, 1), las = 1, bty ="l", cex.axis = 1.1,
       cex.main = 1.5, cex.lab = 2)
   barplot(rbind(tot_prop, nei_prop), beside = T, col = c("blue", "purple"),
@@ -290,11 +425,15 @@ figure_s11 <- function(tot, nei){
   
 }
 
+## Generate the spatial distribution of the latest value of the covariates
 figure_s14 <- function(pop_mat, unvax_mat, cat_incidence1, cat_incidence2, map){
+  # Latest value of the population matrix
   map$pop <- pop_mat[nrow(pop_mat), ][map$nuts3]
+  # Put in categories
   map$pop_cat <- cut(map$pop, breaks = c(0, 300000, 500000, 750000, 1000000, 
                                          max(map$pop, na.rm = T)), 
                      labels = c("70k-300k", "300k-500k", "500k-750k", "750k-1M", ">1M"))
+  # Plot the population categories 
   p_cases1 <- ggplot(map)  +  geom_sf(aes(fill = pop_cat)) +
     scale_fill_manual(na.translate = F, guide = guide_legend(),
                       values = c("#2c7bb6", "#abd9e9", "#ffffbf", "#fdae61", "#d7191c"),
@@ -303,10 +442,14 @@ figure_s14 <- function(pop_mat, unvax_mat, cat_incidence1, cat_incidence2, map){
     theme(axis.text.x = element_blank(), axis.text.y = element_blank(),
           axis.ticks = element_blank(), legend.position = "bottom", 
           axis.line = element_blank()) + guides(fill = guide_legend(ncol = 2))
+  
+  # Latest value of the coverage
   map$cov <- 100 * (1 - exp(unvax_mat[nrow(unvax_mat), ]))[map$nuts3]
+  # Put in categories
   map$cov_cat <- cut(map$cov, breaks = c(0, 80, 85, 90, 95, 
                                          max(map$cov, na.rm = T)), 
                      labels = c("75%-80%", "80%-85%", "85%-90%", "90%-95%", ">95%"))
+  # Plot the coverage categories 
   p_cases2 <- ggplot(map)  +  geom_sf(aes(fill = cov_cat)) +
     scale_fill_manual(na.translate = F, guide = guide_legend(),
                       values = c("#2c7bb6", "#abd9e9", "#ffffbf", "#fdae61", "#d7191c"),
@@ -316,13 +459,16 @@ figure_s14 <- function(pop_mat, unvax_mat, cat_incidence1, cat_incidence2, map){
           axis.ticks = element_blank(), legend.position = "bottom", 
           axis.line = element_blank()) + guides(fill = guide_legend(ncol = 2))
   
+  # Latest value of the category of recent incidence
   map$incidence <- cat_incidence1[nrow(pop_mat), ][map$nuts3] + 
     2*cat_incidence2[nrow(pop_mat), ][map$nuts3]
+  # Name the categories
   map$incidence_cat <- cut(map$incidence, breaks = c(-1, 0, 1, 
                                                      max(map$incidence, na.rm = T)), 
                            labels = c("No recent outbreak", 
                                       "Moderate recent transmission",
                                       "High recent transmission"))
+  # Plot the incidence categories
   p_cases3 <- ggplot(map)  +  geom_sf(aes(fill = incidence_cat)) +
     scale_fill_manual(na.translate = F, guide = guide_legend(),
                       values = c("#ffffbf", "#fdae61", "#d7191c"),
@@ -331,6 +477,7 @@ figure_s14 <- function(pop_mat, unvax_mat, cat_incidence1, cat_incidence2, map){
     theme(axis.text.x = element_blank(), axis.text.y = element_blank(),
           axis.ticks = element_blank(), legend.position = "bottom", 
           axis.line = element_blank()) + guides(fill = guide_legend(ncol = 1))
+  # Put all three figures together
   test_arrange <- arrangeGrob(p_cases1, p_cases2, p_cases3, ncol = 3)
   plot(test_arrange)
 }
