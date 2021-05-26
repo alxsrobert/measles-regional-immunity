507ecdbeec618dbf27dd09e27f34b2056b025c19
alxsrobert <alexis3.robert3@gmail.com>
2021-05-26 15:55:31 +0100

    Comment serial interval, and fix convolution

--- R/function_serial_interval.R
+++ R/function_serial_interval.R
@@ -1,13 +1,13 @@
 conv <- function(w_dens, max_days){
+  # Compute log serial interval
   x <- log(w_dens)
   y <- rev(log(w_dens))
-  
-  n <- length(x)
-  m <- length(y)
+  # Compute the length of the serial interval
+  n <- m <- length(x)
   
   r <- lapply(1:(n+m-1), function(k){
-    i <- 1:max(m,n)
-    i <- i[((i<=m) & ((k-m+i) <= n)) & ((k-m+i) > 0)]
+    i <- seq_len(n)
+    i <- i[(k > (n - i))]
     w <- x[k-m+i]+y[i]
     
     total <- w[1]
@@ -19,17 +19,21 @@ conv <- function(w_dens, max_days){
                                         exp(w[i] - max(total, w[i])))
     }
     return(total)
-  })[1:max_days] %>% unlist %>% exp
-  return(r[seq_along(w_dens)])
+  })[1 + 1:max_days] %>% unlist %>% exp
+  return(r)
 }
 
 serial_interval <- function(prop_gen1, mean_w1, sd_w1, max_days = 50){
-  w1 <- dnorm(x = 1:max_days, mean = mean_w1, sd = sd_w1)
+  # Generate the distribution of the serial interval (no missing case)
+  w1 <- dnorm(x = 0:max_days, mean = mean_w1, sd = sd_w1)
+  # Generate the distribution of the serial interval (missing ancestor)
   w0 <- dnorm(x = 1:max_days, mean = 0, sd = sqrt(2 * sd_w1**2))
+  # Normalise distribution (>= 1 day)
   w0 <- w0/sum(w0)
-  
+  # Generate the distribution of the serial interval (missing generation)
   w2 <- conv(w1, max_days = max_days)
-  
+  w1 <- w1[-1]
+  # Compute the composite serial interval
   w_dens <- w0 * ((1-prop_gen1)/2) + w1 * prop_gen1 + w2 * ((1-prop_gen1)/2)
   return(w_dens)
 }
